24cd96aa80a597ef5c72d0cf5e9ab4330300ae1c
Han Shao <hshao@oberlin.edu>
2020-01-21 01:03:21 +0800

    reuse=true

--- model.py
+++ model.py
@@ -222,7 +222,7 @@ class Discriminator(object):
         if token_type_ids is None:
             token_type_ids = tf.zeros(shape=[batch_size, seq_length], dtype=tf.int32)
 
-        with tf.variable_scope("embeddings"):
+        with tf.variable_scope("embeddings", reuse=True):
             # Perform embedding lookup on the word ids.
             (self.embedding_output, self.embedding_table) = embedding_lookup(
                 input_ids=input_ids,
