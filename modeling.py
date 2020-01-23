2ab4903f1d92c70daca46c81f65f168570fecade
Han Shao <hshao@oberlin.edu>
2020-01-23 20:35:55 +0800

    test

--- modeling.py
+++ modeling.py
@@ -175,9 +175,7 @@ class Generator(object):
                     do_return_all_layers=True)
 
             self.sequence_output = self.all_encoder_layers[-1]
-            print(self.sequence_output)
 
-        model_summary()
 
     def get_sequence_output(self):
         """Gets final hidden layer of encoder.
