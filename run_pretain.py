91361d4785c2950f753a9b35a13e710e8f1728d2
Han Shao <hshao@oberlin.edu>
2020-01-25 01:53:28 +0800

    test variable scope

--- run_pretain.py
+++ run_pretain.py
@@ -101,42 +101,43 @@ def get_masked_lm_output(electra_config, input_tensor, output_weights, positions
     """Get loss and log probs for the masked LM."""
     input_tensor = gather_indexes(input_tensor, positions)
 
-    with tf.variable_scope("cls/predictions"):
-        # We apply one more non-linear transformation before the output layer.
-        # This matrix is not used after pre-training.
-        with tf.variable_scope("transform"):
-            input_tensor = tf.layers.dense(
-                input_tensor,
-                units=electra_config.embedding_size,
-                activation=modeling.get_activation(electra_config.hidden_act),
-                kernel_initializer=modeling.create_initializer(
-                    electra_config.initializer_range))
-            input_tensor = modeling.layer_norm(input_tensor)
+    with tf.variable_scope("generator"):
+        with tf.variable_scope("cls/predictions"):
+            # We apply one more non-linear transformation before the output layer.
+            # This matrix is not used after pre-training.
+            with tf.variable_scope("transform"):
+                input_tensor = tf.layers.dense(
+                    input_tensor,
+                    units=electra_config.embedding_size,
+                    activation=modeling.get_activation(electra_config.hidden_act),
+                    kernel_initializer=modeling.create_initializer(
+                        electra_config.initializer_range))
+                input_tensor = modeling.layer_norm(input_tensor)
 
-        # The output weights are the same as the input embeddings, but there is
-        # an output-only bias for each token.
-        output_bias = tf.get_variable(
-            "output_bias",
-            shape=[electra_config.vocab_size],
-            initializer=tf.zeros_initializer())
-        logits = tf.matmul(input_tensor, output_weights, transpose_b=True)
-        logits = tf.nn.bias_add(logits, output_bias)
-        log_probs = tf.nn.log_softmax(logits, axis=-1)
+            # The output weights are the same as the input embeddings, but there is
+            # an output-only bias for each token.
+            output_bias = tf.get_variable(
+                "output_bias",
+                shape=[electra_config.vocab_size],
+                initializer=tf.zeros_initializer())
+            logits = tf.matmul(input_tensor, output_weights, transpose_b=True)
+            logits = tf.nn.bias_add(logits, output_bias)
+            log_probs = tf.nn.log_softmax(logits, axis=-1)
 
-        label_ids = tf.reshape(label_ids, [-1])
-        label_weights = tf.reshape(label_weights, [-1])
+            label_ids = tf.reshape(label_ids, [-1])
+            label_weights = tf.reshape(label_weights, [-1])
 
-        one_hot_labels = tf.one_hot(
-            label_ids, depth=electra_config.vocab_size, dtype=tf.float32)
+            one_hot_labels = tf.one_hot(
+                label_ids, depth=electra_config.vocab_size, dtype=tf.float32)
 
-        # The `positions` tensor might be zero-padded (if the sequence is too
-        # short to have the maximum number of predictions). The `label_weights`
-        # tensor has a value of 1.0 for every real prediction and 0.0 for the
-        # padding predictions.
-        per_example_loss = -tf.reduce_sum(log_probs * one_hot_labels, axis=[-1])
-        numerator = tf.reduce_sum(label_weights * per_example_loss)
-        denominator = tf.reduce_sum(label_weights) + 1e-5
-        loss = numerator / denominator
+            # The `positions` tensor might be zero-padded (if the sequence is too
+            # short to have the maximum number of predictions). The `label_weights`
+            # tensor has a value of 1.0 for every real prediction and 0.0 for the
+            # padding predictions.
+            per_example_loss = -tf.reduce_sum(log_probs * one_hot_labels, axis=[-1])
+            numerator = tf.reduce_sum(label_weights * per_example_loss)
+            denominator = tf.reduce_sum(label_weights) + 1e-5
+            loss = numerator / denominator
 
     return (loss, per_example_loss, log_probs)
 
