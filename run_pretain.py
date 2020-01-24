7eec644e2a4c838253df1c9630c995cb2498bc02
Han Shao <hshao@oberlin.edu>
2020-01-24 23:00:17 +0800

    test

--- run_pretain.py
+++ run_pretain.py
@@ -184,10 +184,10 @@ def model_fn_builder(electra_config, init_checkpoint, learning_rate,
         zero = tf.constant(0, dtype=tf.int32)
         diff_bool = tf.cast(tf.not_equal(diff, zero), dtype=tf.int32)
 
-        zeros = tf.zeros(tf.shape(input_ids))
+        whether_masked = tf.sparse_to_dense(masked_lm_positions, tf.shape(input_ids), 1, default_value=0, validate_indices=True, name=None)
         print(masked_lm_positions)
         print(input_ids)
-        print(zeros)
+        print(whether_masked)
 
 
 
