ac8477895aad1406d208481e3ca5be3b308fdd93
Han Shao <hshao@oberlin.edu>
2020-01-25 04:27:20 +0800

    test sparse to dense

--- run_pretain.py
+++ run_pretain.py
@@ -222,10 +222,10 @@ def model_fn_builder(electra_config, init_checkpoint, learning_rate,
         diff_cast = tf.cast(tf.not_equal(diff, zero), dtype=tf.int32)
 
         index = tf.expand_dims(tf.range(0, batch_size), 1)
-        dup_index = tf.tile(index, multiples=[1, 20])
+        dup_index = tf.reshape(tf.tile(index, multiples=[1, 20]), [-1])
         print(dup_index)
 
-        positions = tf.concat([dup_index, masked_lm_positions], 1)
+        positions = tf.concat([dup_index, tf.reshape(masked_lm_positions, [-1])], 1)
         print(positions)
 
         whether_replaced = tf.sparse_to_dense(masked_lm_positions, tf.shape(input_ids), diff_cast, default_value=0,
