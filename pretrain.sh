e7cf15c354c639b8c422abd9e0bd20ade4c47e33
Han Shao <hshao@oberlin.edu>
2020-01-26 23:43:47 +0800

    gc

--- pretrain.sh
+++ pretrain.sh
@@ -1,8 +1,9 @@
 TPU_NAME='grpc://10.0.124.242:8470'
+ELECTRA_GC='gs://electra'
 
 python3 run_pretrain.py \
 --input_file=test_256.tfrecord \
---output_dir=results \
+--output_dir=$ELECTRA_GC/results \
 --vocab_file=vocab.txt \
 --do_train=True \
 --train_batch_size=128 \
