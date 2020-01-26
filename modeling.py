790107855516d7e85d2b3cdd0bd56a65b3dab9ea
Han Shao <hshao@oberlin.edu>
2020-01-26 21:59:41 +0800

    add get_assignment_map_from_checkpoint

--- modeling.py
+++ modeling.py
@@ -5,6 +5,8 @@ import json
 import copy
 import numpy as np
 import tensorflow.contrib.slim as slim
+import collections
+import re
 
 
 class ElectraConfig(object):
@@ -405,6 +407,33 @@ def layer_norm(input_tensor, name=None):
     """Run layer normalization on the last dimension of the tensor."""
     return tf.contrib.layers.layer_norm(
         inputs=input_tensor, begin_norm_axis=-1, begin_params_axis=-1, scope=name)
+
+
+def get_assignment_map_from_checkpoint(tvars, init_checkpoint):
+  """Compute the union of the current variables and checkpoint variables."""
+  assignment_map = {}
+  initialized_variable_names = {}
+
+  name_to_variable = collections.OrderedDict()
+  for var in tvars:
+    name = var.name
+    m = re.match("^(.*):\\d+$", name)
+    if m is not None:
+      name = m.group(1)
+    name_to_variable[name] = var
+
+  init_vars = tf.train.list_variables(init_checkpoint)
+
+  assignment_map = collections.OrderedDict()
+  for x in init_vars:
+    (name, var) = (x[0], x[1])
+    if name not in name_to_variable:
+      continue
+    assignment_map[name] = name
+    initialized_variable_names[name] = 1
+    initialized_variable_names[name + ":0"] = 1
+
+  return (assignment_map, initialized_variable_names)
 
 
 def dropout(input_tensor, dropout_prob):
