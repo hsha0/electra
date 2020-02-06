aca89b678cc542a9d2f9a21939c026c145ae0c01
Han Shao <hshao@oberlin.edu>
2020-02-05 19:56:02 -0500

    wiki

--- create_wiki_data.sh
+++ create_wiki_data.sh
@@ -1,4 +1,4 @@
 for num in $(seq 10 99)
 do
-    python3 create_pretraining_data.py --input_file=gs://electra/wiki/processed_wiki/AA/wiki_$num --output_file=gs://electra/data --vocab_file=vocab.txt
+    python3 create_pretraining_data.py --input_file=gs://electra/wiki/processed_wiki/AA/wiki_$num --output_file=gs://electra/data/AA_wiki_$num --vocab_file=vocab.txt
 done
