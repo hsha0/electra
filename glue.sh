5713b847618f076be9a650e7085b072a3482f731
Han Shao <hshao@oberlin.edu>
2020-02-06 15:58:46 -0500

    test glue

---
+++ glue.sh
@@ -0,0 +1,17 @@
+TPU_NAME='grpc://10.1.227.154:8470'
+ELECTRA_GC='gs://electra'
+
+python3 run_classifier.py \
+--task_name=MRPC \
+--data_dir=$ELECTRA_GC/glue_data \
+--output_dir=$ELECTRA_GC/glue_mrpc \
+--init_checkpoint=$ELECTRA_GC/electra_ckpt/model.ckpt-62000
+--vocab_file=vocab.txt \
+--do_train=True \
+--do_eval=True \
+--train_batch_size=32 \
+--learning_rate=2e-5 \
+--max_seq_length=256 \
+--num_train_epochs=3.0 \
+--use_tpu=true \
+--tpu_name=$TPU_NAME
