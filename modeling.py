1f9fc7866934512381692aeb63621e9e6f0ee239
Han Shao <hshao@oberlin.edu>
2020-02-06 16:09:06 -0500

    test glue

--- modeling.py
+++ modeling.py
@@ -28,7 +28,7 @@ class ElectraConfig(object):
                  hidden_act="gelu",
                  hidden_dropout_prob=0.1,
                  attention_probs_dropout_prob=0.1,
-                 max_position_embeddings=256,
+                 max_position_embeddings=512,
                  type_vocab_size=2,
                  initializer_range=0.02):
         """
