e3615488249fe090e796296399bfbe16409f7478
Han Shao <hshao@oberlin.edu>
2020-02-06 18:41:30 -0500

    test sigmoid

--- pretrain.sh
+++ pretrain.sh
@@ -3,7 +3,7 @@ ELECTRA_GC='gs://electra'
 
 python3 run_pretrain.py \
 --input_file=$ELECTRA_GC/data/*.tfrecord \
---output_dir=$ELECTRA_GC/electra_pretrain_models \
+--output_dir=$ELECTRA_GC/electra_sigmoid \
 --vocab_file=vocab.txt \
 --do_train=True \
 --train_batch_size=128 \
