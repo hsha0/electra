4950ed6401c41e6954f7d3721614e6195c11a455
Han Shao <hshao@oberlin.edu>
2020-02-07 15:55:57 -0500

    2.0

--- modeling.py
+++ modeling.py
@@ -412,7 +412,7 @@ def get_shape_list(tensor, expected_rank=None, name=None):
 
 def layer_norm(input_tensor, name=None):
     """Run layer normalization on the last dimension of the tensor."""
-    return tf.contrib.layers.layer_norm(
+    return tf.keras.layers.LayerNormalization(
         inputs=input_tensor, begin_norm_axis=-1, begin_params_axis=-1, scope=name)
 
 
