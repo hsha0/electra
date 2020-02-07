6199ff694b64a262367df4ed50b5ab852c4efa18
Han Shao <hshao@oberlin.edu>
2020-02-07 12:50:10 -0500

    lamb

--- pretrain.sh
+++ pretrain.sh
@@ -3,11 +3,11 @@ ELECTRA_GC='gs://electra'
 
 python3 run_pretrain.py \
 --input_file=$ELECTRA_GC/data/*.tfrecord \
---output_dir=$ELECTRA_GC/electra_sigmoid \
+--output_dir=$ELECTRA_GC/electra_lamb \
 --vocab_file=vocab.txt \
 --do_train=True \
 --train_batch_size=128 \
 --max_seq_length=256 \
---num_train_steps=1000000 \
+--num_train_steps=62500 \
 --use_tpu=true \
 --tpu_name=$TPU_NAME
