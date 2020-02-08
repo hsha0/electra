dc4b74b0f7e0059940b82e6241ee93304bd28485
Han Shao <hshao@oberlin.edu>
2020-02-08 10:50:34 -0500

    test

--- create_book_data.sh
+++ create_book_data.sh
@@ -1,4 +1,4 @@
 for num in $(seq 1 22)
 do
-    python3 create_pretraining_data.py --input_file=../bucket/bookcorpus/processed_book/bookcorpus_$num.txt --output_file=../bucket/data/bookcorpus_$num.tfrecord --vocab_file=vocab.txt
+    python3 create_pretraining_data.py --input_file=../bucket/bookcorpus/processed_book/bookcorpus_$num.txt --output_file=../bucket/data_128/bookcorpus_$num.tfrecord --vocab_file=vocab.txt
 done
