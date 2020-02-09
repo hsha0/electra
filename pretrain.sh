98d00e0cdc3227b635eafc11abcb9c24f9ee68ba
Han Shao <hshao@oberlin.edu>
2020-02-08 21:59:30 -0500

    128

--- pretrain.sh
+++ pretrain.sh
@@ -1,13 +1,13 @@
-TPU_NAME='grpc://10.58.185.146:8470'
+TPU_NAME='grpc://10.78.108.10:8470'
 ELECTRA_GC='gs://electra'
 
 python3 run_pretrain.py \
---input_file=$ELECTRA_GC/data/*.tfrecord \
---output_dir=$ELECTRA_GC/electra_lamb_ \
+--input_file=$ELECTRA_GC/data_128/*.tfrecord \
+--output_dir=$ELECTRA_GC/electra_lamb_128 \
 --vocab_file=vocab.txt \
 --do_train=True \
 --train_batch_size=128 \
 --max_seq_length=256 \
---num_train_steps=1000000 \
+--num_train_steps=62500 \
 --use_tpu=true \
 --tpu_name=$TPU_NAME
