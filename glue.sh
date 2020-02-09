769c8f2aa8b5578b8e22ef5cd7bea28c651b3f1e
Han Shao <hshao@oberlin.edu>
2020-02-09 14:18:14 -0500

    glue

--- glue.sh
+++ glue.sh
@@ -1,4 +1,4 @@
-TPU_NAME='grpc://10.97.148.162:8470'
+TPU_NAME='grpc://10.92.203.170:8470'
 ELECTRA_GC='gs://electra'
 TASK=MNLI
 
@@ -6,7 +6,7 @@ python3 run_classifier.py \
 --task_name=$TASK \
 --data_dir=$ELECTRA_GC/glue/glue_data/$TASK \
 --output_dir=$ELECTRA_GC/glue/glue_results/glue_$TASK \
---init_checkpoint=$ELECTRA_GC/electra_ckpt/model.ckpt-125000 \
+--init_checkpoint=$ELECTRA_GC/electra_128_ckpt/model.ckpt-62500 \
 --vocab_file=vocab.txt \
 --do_train=True \
 --do_eval=True \
