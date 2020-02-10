aa62b3a21109806fd8912a37e815ef7956ffb2bc
Han Shao <hshao@oberlin.edu>
2020-02-09 19:15:58 -0500

    glue

--- glue.sh
+++ glue.sh
@@ -6,7 +6,7 @@ python3 run_classifier.py \
 --task_name=$TASK \
 --data_dir=$ELECTRA_GC/glue/glue_data/$TASK \
 --output_dir=$ELECTRA_GC/glue/glue_results/glue_$TASK \
---init_checkpoint=$ELECTRA_GC/electra_pretrain/electra_gen_0.5_ckpt/model.ckpt-125000 \
+--init_checkpoint=$ELECTRA_GC/electra_pretrain/electra_gen_0.5_ckpt/model.ckpt-168000 \
 --vocab_file=vocab.txt \
 --do_train=True \
 --do_eval=True \
