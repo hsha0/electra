b03abb076604e9a39aed7a58e42a23bb011e1c45
Han Shao <hshao@oberlin.edu>
2020-02-09 20:41:10 -0500

    1e-4

--- pretrain.sh
+++ pretrain.sh
@@ -9,7 +9,7 @@ python3 run_pretrain.py \
 --learning_rate=1e-4 \
 --train_batch_size=128 \
 --max_seq_length=128 \
---num_train_steps=500000 \
+--num_train_steps=62500 \
 --max_predictions_per_seq=20 \
 --use_tpu=true \
 --tpu_name=$TPU_NAME
