3bf48789ef790598dc9d26e50eee2062857f8e6e
Han Shao <hshao@oberlin.edu>
2020-02-10 12:29:15 -0500

    logits shape

--- pretrain.sh
+++ pretrain.sh
@@ -1,12 +1,12 @@
-TPU_NAME='grpc://10.22.1.162:8470'
+TPU_NAME='grpc://10.37.248.170:8470'
 ELECTRA_GC='gs://electra'
 
 python3 run_pretrain.py \
 --input_file=$ELECTRA_GC/data_128/*.tfrecord \
---output_dir=$ELECTRA_GC/electra_pretrain/electra_5e-4 \
+--output_dir=$ELECTRA_GC/electra_pretrain/electra_6e-4 \
 --vocab_file=vocab.txt \
 --do_train=True \
---learning_rate=5e-4 \
+--learning_rate=6e-4 \
 --train_batch_size=128 \
 --max_seq_length=128 \
 --num_train_steps=62500 \
--- run_pretrain.py
+++ run_pretrain.py
@@ -131,6 +131,8 @@ def get_masked_lm_output(electra_config, input_tensor, output_weights, positions
             logits = tf.matmul(input_tensor, output_weights, transpose_b=True)
             logits = tf.nn.bias_add(logits, output_bias)
             log_probs = tf.nn.log_softmax(logits, axis=-1)
+            print(log_probs)
+            sys.exit()
 
             label_ids = tf.reshape(label_ids, [-1])
             label_weights = tf.reshape(label_weights, [-1])
