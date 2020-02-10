5834bb5a43cd8e9180698f6f446ff2692a480e8a
Han Shao <hshao@oberlin.edu>
2020-02-09 21:24:48 -0500

    2e-4

--- pretrain.sh
+++ pretrain.sh
@@ -3,10 +3,10 @@ ELECTRA_GC='gs://electra'
 
 python3 run_pretrain.py \
 --input_file=$ELECTRA_GC/data_128/*.tfrecord \
---output_dir=$ELECTRA_GC/electra_pretrain/electra_1e-4 \
+--output_dir=$ELECTRA_GC/electra_pretrain/electra_2e-4 \
 --vocab_file=vocab.txt \
 --do_train=True \
---learning_rate=1e-4 \
+--learning_rate=2e-4 \
 --train_batch_size=128 \
 --max_seq_length=128 \
 --num_train_steps=62500 \
