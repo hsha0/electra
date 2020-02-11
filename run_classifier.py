a19319451bcd93659232cf35e839d0d341403728
Han Shao <hshao@oberlin.edu>
2020-02-11 18:35:31 -0500

    glue

--- run_classifier.py
+++ run_classifier.py
@@ -565,13 +565,11 @@ class WNLIProcessor(DataProcessor):
     """Processor for the WNLI data set (GLUE version)."""
     def get_train_examples(self, data_dir):
         return self._create_examples(
-            self._read_tsv(os.path.join(data_dir, "train.tsv"), "train")
-        )
+            self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
 
     def get_dev_examples(self, data_dir):
         return self._create_examples(
-            self._read_tsv(os.path.join(data_dir, "dev.tsv"), "dev")
-        )
+            self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
 
     def get_test_examples(self, data_dir):
         """See base class."""
