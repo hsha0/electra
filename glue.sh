31ec6d2ad616bb21b3e57d4793b2e88b58605a29
Han Shao <hshao@oberlin.edu>
2020-02-11 20:05:55 -0500

    glue

--- glue.sh
+++ glue.sh
@@ -1,6 +1,6 @@
 TPU_NAME='grpc://10.57.129.2:8470'
 ELECTRA_GC='gs://electra'
-TASK=CoLA
+TASK=SST-2
 
 python3 run_classifier.py \
 --task_name=$TASK \
@@ -8,12 +8,12 @@ python3 run_classifier.py \
 --output_dir=$ELECTRA_GC/glue/glue_results/step1M/$TASK \
 --init_checkpoint=$ELECTRA_GC/electra_pretrain/electra_5e-4_CLS_0.25/model.ckpt-1000000 \
 --vocab_file=vocab.txt \
---do_train=False \
+--do_train=True \
 --do_eval=True \
 --train_batch_size=32 \
 --learning_rate=3e-4 \
 --max_seq_length=128 \
 --num_train_epochs=3.0 \
 --seed=12345 \
---use_tpu=False \
+--use_tpu=True \
 --tpu_name=$TPU_NAME
