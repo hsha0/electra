028cd00b9eb7d274cd6128720a575681105a14d4
Han Shao <hshao@oberlin.edu>
2020-02-12 12:35:15 -0500

    pretrain_1024

--- pretrain.sh
+++ pretrain.sh
@@ -1,17 +1,17 @@
-TPU_NAME='grpc://10.70.112.18:8470'
+TPU_NAME='grpc://10.71.239.122:8470'
 ELECTRA_GC='gs://electra'
 
 python3 run_pretrain.py \
---input_file=$ELECTRA_GC/data_128_CLS/*.tfrecord \
---output_dir=$ELECTRA_GC/electra_pretrain/electra_5e-4_CLS_seg_clip \
+--input_file=$ELECTRA_GC/data_128_sent_CLS/*.tfrecord \
+--output_dir=$ELECTRA_GC/electra_pretrain/electra_5e-4_bz1024 \
 --vocab_file=vocab.txt \
 --do_train=True \
 --learning_rate=5e-4 \
---train_batch_size=128 \
+--train_batch_size=1024 \
 --max_seq_length=128 \
---num_train_steps=1000000 \
+--num_train_steps=125000 \
 --max_predictions_per_seq=20 \
---save_checkpoints_steps=6250 \
---iterations_per_loop=6250 \
+--save_checkpoints_steps=1000 \
+--iterations_per_loop=1000 \
 --use_tpu=true \
 --tpu_name=$TPU_NAME
