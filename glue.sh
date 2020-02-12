b05fe74bacbbb8c9c5f2aba51aa1aeca0752fec0
Han Shao <hshao@oberlin.edu>
2020-02-12 18:34:03 -0500

    glue 1024

--- glue.sh
+++ glue.sh
@@ -1,19 +1,19 @@
 TPU_NAME='grpc://10.57.129.2:8470'
 ELECTRA_GC='gs://electra'
-TASK=RTE
+TASK=MRPC
 
 python3 run_classifier.py \
 --task_name=$TASK \
 --data_dir=$ELECTRA_GC/glue/glue_data/$TASK \
---output_dir=$ELECTRA_GC/glue/glue_results/step1M/$TASK \
---init_checkpoint=$ELECTRA_GC/electra_pretrain/electra_5e-4_CLS_0.25/model.ckpt-1000000 \
+--output_dir=$ELECTRA_GC/glue/glue_results/bz1024/$TASK \
+--init_checkpoint=$ELECTRA_GC/electra_pretrain/electra_5e-4_bz1024/model.ckpt-125000 \
 --vocab_file=vocab.txt \
 --do_train=True \
 --do_eval=True \
 --train_batch_size=32 \
 --learning_rate=3e-4 \
 --max_seq_length=128 \
---num_train_epochs=10.0 \
+--num_train_epochs=3.0 \
 --seed=12345 \
---use_tpu=True \
+--use_tpu=False \
 --tpu_name=$TPU_NAME
