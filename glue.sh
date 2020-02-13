b0ba2ddbda631c4f52ad3b305e420c08650019fd
Han Shao <hshao@oberlin.edu>
2020-02-13 16:35:31 -0500

    glue

--- glue.sh
+++ glue.sh
@@ -11,8 +11,8 @@ python3 run_classifier.py \
 --vocab_file=vocab.txt \
 --do_train=True \
 --do_eval=True \
---train_batch_size=32 \
---learning_rate=3e-4 \
+--train_batch_size=128 \
+--learning_rate=3e-5 \
 --max_seq_length=128 \
 --num_train_epochs=3.0 \
 --seed=$1 \
