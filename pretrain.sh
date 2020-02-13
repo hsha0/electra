823f2363160870df1a356aaf7e62f123f7841654
Han Shao <hshao@oberlin.edu>
2020-02-13 03:20:22 -0500

    test pretrain

--- pretrain.sh
+++ pretrain.sh
@@ -3,15 +3,15 @@ ELECTRA_GC='gs://electra'
 
 python3 run_pretrain.py \
 --input_file=$ELECTRA_GC/data_128_sent_CLS/*.tfrecord \
---output_dir=$ELECTRA_GC/electra_pretrain/electra_h256_bz1920_lr0.001 \
+--output_dir=$ELECTRA_GC/electra_pretrain/electra_h256_bz1024_lr0.001 \
 --vocab_file=vocab.txt \
 --do_train=True \
 --learning_rate=0.001 \
---train_batch_size=1920 \
+--train_batch_size=1024 \
 --max_seq_length=128 \
---num_train_steps=250000 \
+--num_train_steps=125000 \
 --max_predictions_per_seq=20 \
---save_checkpoints_steps=6250 \
---iterations_per_loop=6250 \
+--save_checkpoints_steps=1000 \
+--iterations_per_loop=1000 \
 --use_tpu=true \
 --tpu_name=$TPU_NAME
