f36b32c3d6f8ac4286cd8a6aaed2d60ecfbf9fdc
Han Shao <hshao@oberlin.edu>
2020-02-12 21:32:02 -0500

    glue_bert

--- glue.sh
+++ glue.sh
@@ -1,12 +1,14 @@
 TPU_NAME='grpc://10.8.246.2:8470'
 ELECTRA_GC='gs://electra'
 TASK=MNLI
+# INIT_CKPT=$ELECTRA_GC/electra_pretrain/electra_5e-4_bz1024/model.ckpt-125000
+INIT_CKPT=gs://bert_sh/bert_pretrain/bert_5e-4/model.ckpt-87500
 
 python3 run_classifier.py \
 --task_name=$TASK \
 --data_dir=$ELECTRA_GC/glue/glue_data/$TASK \
---output_dir=$ELECTRA_GC/glue/glue_results/bz1024/$TASK \
---init_checkpoint=$ELECTRA_GC/electra_pretrain/electra_5e-4_bz1024/model.ckpt-125000 \
+--output_dir=$ELECTRA_GC/glue/glue_results/bert_small/$TASK \
+--init_checkpoint= $INIT_CKPT \
 --vocab_file=vocab.txt \
 --do_train=True \
 --do_eval=True \
