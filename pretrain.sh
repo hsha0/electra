6b2d60ede957b437b8574f99e3f40e2894d03a04
Han Shao <hshao@oberlin.edu>
2020-02-14 14:27:24 -0500

    test pb

--- pretrain.sh
+++ pretrain.sh
@@ -1,9 +1,10 @@
-TPU_NAME='grpc://10.68.243.130:8470'
+TPU_NAME='grpc://10.43.245.90:8470'
 ELECTRA_GC='gs://electra'
 MODEL=albert
 CONFIG='config/ale_small.json'
 
 python3 run_pretrain.py \
+--electra_config_file=$CONFIG \
 --input_file=$ELECTRA_GC/data_128_sent_CLS/*.tfrecord \
 --output_dir=$ELECTRA_GC/electra_pretrain/ele${MODEL}_h256_l128 \
 --vocab_file=vocab.txt \
@@ -12,7 +13,7 @@ python3 run_pretrain.py \
 --learning_rate=5e-5 \
 --train_batch_size=1024 \
 --max_seq_length=128 \
---num_train_steps=87500 \
+--num_train_steps=1 \
 --max_predictions_per_seq=20 \
 --save_checkpoints_steps=1000 \
 --iterations_per_loop=1000 \
