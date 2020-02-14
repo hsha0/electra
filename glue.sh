d73722d312eade109c5ad16e47de421567f8a538
Han Shao <hshao@oberlin.edu>
2020-02-13 23:42:45 -0500

    test 1024

--- glue.sh
+++ glue.sh
@@ -2,12 +2,12 @@ TPU_NAME='grpc://10.91.4.82:8470'
 ELECTRA_GC='gs://electra'
 #INIT_CKPT=$ELECTRA_GC/electra_pretrain/electra_h1024_w50/model.ckpt-15000
 MODEL=albert
-TASK=MNLI
+TASK=MRPC
 
 python3 run_classifier.py \
 --task_name=$TASK \
 --data_dir=$ELECTRA_GC/glue/glue_data/$TASK \
---output_dir=$ELECTRA_GC/glue/glue_results/h1024_w50/$TASK \
+--output_dir=$ELECTRA_GC/glue/glue_results/h1024_0/$TASK \
 --init_checkpoint=$INIT_CKPT \
 --vocab_file=vocab.txt \
 --model=$MODEL \
