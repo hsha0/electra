1c608ac6ddf7780130c107d838567a5380c4bb79
Han Shao <hshao@oberlin.edu>
2020-02-13 22:00:10 -0500

    test 1024

--- glue.sh
+++ glue.sh
@@ -14,7 +14,7 @@ python3 run_classifier.py \
 --do_train=True \
 --do_eval=True \
 --train_batch_size=128 \
---learning_rate=3e-5 \
+--learning_rate=5e-5 \
 --max_seq_length=128 \
 --num_train_epochs=3.0 \
 --seed=$1 \
