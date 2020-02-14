6e6623b115dacda3db81be13fab5ef23fa5a94ac
Han Shao <hshao@oberlin.edu>
2020-02-14 15:05:53 -0500

    test pb

--- pretrain.sh
+++ pretrain.sh
@@ -1,18 +1,22 @@
 TPU_NAME='grpc://10.43.245.90:8470'
+MODEL=electra
+SIZE=small
+MAX_SEQ_L=128
+LR=5e-5
+
 ELECTRA_GC='gs://electra'
-MODEL=ale
-CONFIG='config/ale_small.json'
+CONFIG=config/${MODEL}_${SIZE}.json
 
 python3 run_pretrain.py \
 --electra_config_file=$CONFIG \
 --input_file=$ELECTRA_GC/data_128_sent_CLS/*.tfrecord \
---output_dir=$ELECTRA_GC/electra_pretrain/${MODEL}_h256_l128 \
+--output_dir=$ELECTRA_GC/electra_pretrain/${MODEL}_${SIZE}_seq${MAX_SEQ_L}_lr${LR} \
 --vocab_file=vocab.txt \
 --model=$MODEL \
 --do_train=True \
---learning_rate=5e-5 \
+--learning_rate=${LR} \
 --train_batch_size=1024 \
---max_seq_length=128 \
+--max_seq_length=${MAX_SEQ_L} \
 --num_train_steps=1 \
 --max_predictions_per_seq=20 \
 --save_checkpoints_steps=1000 \
