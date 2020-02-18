1328ccf899dd216767c550a4812008602149fd1f
Han Shao <hshao@oberlin.edu>
2020-02-18 13:29:31 -0500

    test mcc

--- albert_modeling.py
+++ albert_modeling.py
@@ -30,7 +30,6 @@ import re
 import numpy as np
 import six
 from six.moves import range
-from tensorflow.contrib import layers as contrib_layers
 import tensorflow as tf
 
 
@@ -581,8 +580,7 @@ def dropout(input_tensor, dropout_prob):
 
 def layer_norm(input_tensor, name=None):
   """Run layer normalization on the last dimension of the tensor."""
-  return contrib_layers.layer_norm(
-      inputs=input_tensor, begin_norm_axis=-1, begin_params_axis=-1, scope=name)
+  return tf.keras.layers.LayerNormalization(axis=-1, epsilon=1e-12, name='layer_norm')(input_tensor)
 
 
 def layer_norm_and_dropout(input_tensor, dropout_prob, name=None):
