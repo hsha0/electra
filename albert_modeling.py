bc0b518efd96cfea8d8229855681e1c4dab6d693
Han Shao <hshao@oberlin.edu>
2020-02-18 19:17:28 -0500

    revert

--- albert_modeling.py
+++ albert_modeling.py
@@ -581,7 +581,8 @@ def dropout(input_tensor, dropout_prob):
 
 def layer_norm(input_tensor, name=None):
   """Run layer normalization on the last dimension of the tensor."""
-  return tf.keras.layers.LayerNormalization(axis=-1, epsilon=1e-12, name='layer_norm')(input_tensor)
+  return contrib_layers.layer_norm(
+      inputs=input_tensor, begin_norm_axis=-1, begin_params_axis=-1, scope=name)
 
 
 def layer_norm_and_dropout(input_tensor, dropout_prob, name=None):
