d19e61356b6cfb7245b8047f8ecc9e88c0270ac6
Han Shao <hshao@oberlin.edu>
2020-02-19 16:20:38 -0500

    test ele small

--- pretrain.sh
+++ pretrain.sh
@@ -1,24 +1,25 @@
 TPU_NAME='grpc://10.125.243.18:8470'
 MODEL=electra
 SIZE=small
-MAX_SEQ_L=128
-LR=5e-4
+LR=3e-4
 DISC_W=1
 TRAIN_STEP=62500
+BZ=128
 
 ELECTRA_GC='gs://electra'
 CONFIG=config/${MODEL}_${SIZE}.json
+MAX_SEQ_L=128
 
 python3 run_pretrain.py \
 --electra_config_file=$CONFIG \
 --input_file=$ELECTRA_GC/data_128_sent_CLS/*.tfrecord \
---output_dir=$ELECTRA_GC/electra_pretrain/${MODEL}_${SIZE}_seq${MAX_SEQ_L}_lr${LR}_w${DISC_W} \
+--output_dir=$ELECTRA_GC/electra_pretrain/${MODEL}_${SIZE}_seq${MAX_SEQ_L}_lr${LR}_w${DISC_W}_bz${BZ} \
 --vocab_file=vocab.txt \
 --disc_loss_weight=${DISC_W} \
 --model=$MODEL \
 --do_train=True \
 --learning_rate=${LR} \
---train_batch_size=128 \
+--train_batch_size=${BZ} \
 --max_seq_length=${MAX_SEQ_L} \
 --num_train_steps=${TRAIN_STEP} \
 --max_predictions_per_seq=20 \
