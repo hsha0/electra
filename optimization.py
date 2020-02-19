18764667c3df4d36180d825dadb1881c3d27a03b
Han Shao <hshao@oberlin.edu>
2020-02-19 17:15:24 -0500

    test ele small

--- optimization.py
+++ optimization.py
@@ -90,12 +90,14 @@ def create_optimizer(loss, init_lr, num_train_steps, num_warmup_steps, use_tpu, 
   grads = tf.gradients(loss, tvars)
   print(len(grads))
   print(len(tvars))
-  if use_tpu:
-      grads = [tf.compat.v1.tpu.cross_replica_sum(grad) for grad in grads if (grad is not None)]
-
   for grad, var in zip(grads, tvars):
       if grad is None:
           print(var)
+
+  if use_tpu:
+      grads = [tf.compat.v1.tpu.cross_replica_sum(grad) for grad in grads if (grad is not None)]
+
+
   # This is how the model was pre-trained.
   (grads, _) = tf.clip_by_global_norm(grads, clip_norm=1.0)
 
