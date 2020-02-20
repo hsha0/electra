ae8e5a3624e421fb1c1076624bedb6ad884ee1be
Han Shao <hshao@oberlin.edu>
2020-02-20 03:09:46 -0500

    test ele small

--- run_pretrain.py
+++ run_pretrain.py
@@ -365,12 +365,13 @@ def model_fn_builder(electra_config, init_checkpoint, learning_rate,
                 scaffold_fn=scaffold_fn,
             )
 
-
+            '''
             flops = tf.profiler.profile(
                 tf.get_default_graph(),
                 options=tf.profiler.ProfileOptionBuilder.float_operation())
             print(flops.total_float_ops, '\n\n\n')
             sys.exit()
+            '''
         return output_spec
 
     return model_fn
