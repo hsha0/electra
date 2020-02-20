825b26a4bce806a7904f0a55851374642ffa51eb
Han Shao <hshao@oberlin.edu>
2020-02-20 01:31:42 -0500

    test ele small

--- glue.sh
+++ glue.sh
@@ -8,17 +8,17 @@ SEED=654
 TASK_INDEX=2
 
 TASKS=(MRPC CoLA MNLI SST-2 QQP QNLI WNLI RTE STS-B)
-LRS=(3e-4 3e-4 3e-4 3e-4 3e-4 3e-4 3e-4 3e-4 3e-4)
-#LRS=(2e-5 1e-5 3e-5 1e-5 5e-5 1e-5 2e-5 3e-5 2e-5)
-BZS=(32 32 32 32 32 32 32 32 32)
-#BZS=(32 16 128 32 128 32 16 32 16)
+#LRS=(3e-4 3e-4 3e-4 3e-4 3e-4 3e-4 3e-4 3e-4 3e-4)
+LRS=(2e-5 1e-5 3e-5 1e-5 5e-5 1e-5 2e-5 3e-5 2e-5)
+#BZS=(32 32 32 32 32 32 32 32 32)
+BZS=(32 16 128 32 128 32 16 32 16)
 EPOCHS=(3 3 3 3 3 3 3 10 10)
 
 TASK=${TASKS[${TASK_INDEX}]}
 LR=${LRS[${TASK_INDEX}]}
 BZ=${BZS[${TASK_INDEX}]}
 EPOCH=${EPOCHS[${TASK_INDEX}]}
-INIT_CKPT=$ELECTRA_GC/electra_pretrain/electra_small_seq128_lr5e-4_w50_bz1024/model.ckpt-${CKPT}
+INIT_CKPT=$ELECTRA_GC/electra_pretrain/electra_small_seq128_lr0.01_w20_bz1024/model.ckpt-${CKPT}
 
 CONFIG=config/${MODEL}_${SIZE}.json
 
