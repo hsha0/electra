9866ab1225fa6553ce1129c835960935ad9cc8ca
Han Shao <hshao@oberlin.edu>
2020-02-19 21:39:48 -0500

    test ele small

--- run_pretrain.py
+++ run_pretrain.py
@@ -194,8 +194,7 @@ def get_discriminator_output(electra_config, sequence_tensor, whether_replaced, 
 
             #per_example_loss = tf.reduce_sum(input_tensor=tf.multiply(sigmoid_cross_entropy, one_hot_labels), axis=[-1])
             per_example_loss = sigmoid_cross_entropy
-            print(per_example_loss)
-            sys.exit()
+
             label_weights = tf.reshape(tf.cast(label_weights, tf.float32), [-1])
             numerator = tf.reduce_sum(label_weights * per_example_loss)
             denominator = tf.reduce_sum(label_weights) + 1e-5
