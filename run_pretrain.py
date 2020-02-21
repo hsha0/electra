bd0df2bea6fd42785a786e026054440a96c67b00
Han Shao <hshao@oberlin.edu>
2020-02-20 23:06:37 -0500

    test sigmoid cross entropy

--- run_pretrain.py
+++ run_pretrain.py
@@ -222,7 +222,9 @@ def get_discriminator_output(electra_config, sequence_tensor, whether_replaced, 
             whether_replaced = tf.cast(tf.reshape(whether_replaced, [batch_size * seq_length, 1]), tf.float32)
             per_example_loss = tf.multiply(tf.log(tf.sigmoid(logits)), whether_replaced) + tf.multiply(
                                                     (1 - whether_replaced),tf.log(1 - tf.sigmoid(logits)))
-            loss = tf.reduce_sum(per_example_loss) / (batch_size * seq_length)
+            weighted_per_example_loss = label_weights * per_example_loss
+            print(weighted_per_example_loss)
+            sys.exit()
 
 
     return (loss)
