fe56d6529ce6d3b6b294953d100d92e2e3a94a99
Han Shao <hshao@oberlin.edu>
2020-02-23 22:40:32 -0500

    test adam for glue

--- run_classifier.py
+++ run_classifier.py
@@ -997,6 +997,7 @@ def model_fn_builder(electra_config, num_labels, init_checkpoint, learning_rate,
 
             mcc = MatthewsCorrelationCoefficient(num_classes=1)
             mcc.update_state(tf.cast(label_ids, tf.float32), tf.cast(predictions, tf.float32))
+            mcc = tf.compat.v1.metrics.mean(mcc.result())
             #mcc = mcc_metric(y_true=label_ids, y_pred=predictions)
             #mcc = tf.reshape(mcc, [1])
             #print(mcc)
