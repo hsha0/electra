1dae7ed376d63b60b61f98b91bf458f7ed602693
Han Shao <hshao@oberlin.edu>
2020-02-23 23:12:32 -0500

    test adam for glue

--- run_classifier.py
+++ run_classifier.py
@@ -901,14 +901,35 @@ def create_model(electra_config, is_training, input_ids, input_mask, segment_ids
 
 
 def mcc_metric(y_true, y_pred):
+  '''
   true_pos = tf.math.count_nonzero(y_pred * y_true)
   true_neg = tf.math.count_nonzero((y_pred - 1) * (y_true - 1))
   false_pos = tf.math.count_nonzero(y_pred * (y_true - 1))
   false_neg = tf.math.count_nonzero((y_pred - 1) * y_true)
   x = tf.cast((true_pos + false_pos) * (true_pos + false_neg)
       * (true_neg + false_pos) * (true_neg + false_neg), tf.float32)
-  return tf.cast((true_pos * true_neg) - (false_pos * false_neg), tf.float32) / tf.sqrt(x)
+  '''
+  y_true = tf.cast(y_true, dtype=self.dtype)
+  y_pred = tf.cast(y_pred, dtype=self.dtype)
 
+  true_positive = tf.math.count_nonzero(y_true * y_pred, 0)
+  # true_negative
+  y_true_negative = tf.math.not_equal(y_true, 1.0)
+  y_pred_negative = tf.math.not_equal(y_pred, 1.0)
+  true_negative = tf.math.count_nonzero(
+      tf.math.logical_and(y_true_negative, y_pred_negative), axis=0
+  )
+  # predicted sum
+  pred_sum = tf.math.count_nonzero(y_pred, 0)
+  # Ground truth label sum
+  true_sum = tf.math.count_nonzero(y_true, 0)
+  false_positive = pred_sum - true_positive
+  false_negative = true_sum - true_positive
+
+  #return tf.cast((true_pos * true_neg) - (false_pos * false_neg), tf.float32) / tf.sqrt(x)
+  x = tf.cast((true_positive + false_positive) * (true_positive + false_negative)
+      * (true_negative + false_positive) * (true_negative + false_negative), tf.float32)
+  return tf.cast((true_positive * true_negative) - (false_positive * false_negative), tf.float32) / tf.sqrt(x)
 
 def model_fn_builder(electra_config, num_labels, init_checkpoint, learning_rate,
                      num_train_steps, num_warmup_steps, use_tpu,
@@ -995,10 +1016,10 @@ def model_fn_builder(electra_config, num_labels, init_checkpoint, learning_rate,
             accuracy = tf.compat.v1.metrics.accuracy(
                 labels=label_ids, predictions=predictions, weights=is_real_example)
 
-            mcc = MatthewsCorrelationCoefficient(num_classes=1)
-            mcc.update_state(label_ids, predictions)
-            mcc = mcc.result()
-            #mcc = mcc_metric(y_true=label_ids, y_pred=predictions)
+            #mcc = MatthewsCorrelationCoefficient(num_classes=1)
+            #mcc.update_state(label_ids, predictions)
+            #mcc = mcc.result()
+            mcc = mcc_metric(y_true=label_ids, y_pred=predictions)
             mcc = tf.compat.v1.metrics.mean(values=mcc)
             return {
                 "eval_mcc": mcc,
