bc355b4482246afcb45bcb7c68383b3312c9387a
Han Shao <hshao@oberlin.edu>
2020-02-25 05:40:27 -0500

    test 4e-3

--- pretrain.sh
+++ pretrain.sh
@@ -1,11 +1,12 @@
 TPU_NAME='grpc://10.20.98.122:8470'
 MODEL=electra
 SIZE=small
-LR=5e-4
+LR=4e-3
 DISC_W=50
-TRAIN_STEP=62500
-TOTAL=1000000
-BZ=128
+TRAIN_STEP=125000
+TOTAL=125000
+WARM_UP=1250
+BZ=1024
 
 ELECTRA_GC='gs://electra'
 CONFIG=config/${MODEL}_${SIZE}.json
@@ -14,10 +15,11 @@ MAX_SEQ_L=128
 python3 run_pretrain.py \
 --electra_config_file=$CONFIG \
 --input_file=$ELECTRA_GC/data_128_sent_CLS/*.tfrecord \
---output_dir=$ELECTRA_GC/electra_pretrain/${MODEL}_${SIZE}_seq${MAX_SEQ_L}_lr${LR}_w${DISC_W}_bz${BZ}_1M \
+--output_dir=$ELECTRA_GC/electra_pretrain/${MODEL}_${SIZE}_seq${MAX_SEQ_L}_lr${LR}_w${DISC_W}_bz${BZ} \
 --vocab_file=vocab.txt \
 --disc_loss_weight=${DISC_W} \
 --model=$MODEL \
+--num_warmup_steps=${WARM_UP} \
 --do_train=True \
 --learning_rate=${LR} \
 --train_batch_size=${BZ} \
@@ -27,5 +29,5 @@ python3 run_pretrain.py \
 --max_predictions_per_seq=20 \
 --save_checkpoints_steps=1000 \
 --iterations_per_loop=1000 \
---use_tpu=False \
+--use_tpu=True \
 --tpu_name=$TPU_NAME
