f5b084bd92113e0a1b712de82c4e0148e95d0282
Han Shao <hshao@oberlin.edu>
2020-02-25 20:04:21 -0500

    test qqp

--- run_classifier.py
+++ run_classifier.py
@@ -1208,7 +1208,7 @@ def main(_):
   num_train_steps = None
   num_warmup_steps = None
   #tfrecord_tasks = ['mnli', 'sst-2', 'qqp', 'qnli']
-  tfrecord_tasks = ['mnli', 'sst-2']
+  tfrecord_tasks = ['mnli', 'sst-2', 'qqp']
   if FLAGS.do_train:
     if task_name in tfrecord_tasks:
         num_examples = processor.get_examples_num()
