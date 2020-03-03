a7cc69e066a87bd569e2abae6815cb58319b2623
Han Shao <hshao@oberlin.edu>
2020-03-03 15:21:18 -0500

    ele small sampling

--- pretrain.sh
+++ pretrain.sh
@@ -16,7 +16,7 @@ MAX_SEQ_L=128
 python3 run_pretrain.py \
 --electra_config_file=$CONFIG \
 --input_file=$ELECTRA_GC/data_128_sent_CLS/*.tfrecord \
---output_dir=$ELECTRA_GC/electra_pretrain/${MODEL}_${SIZE}_seq${MAX_SEQ_L}_lr${LR}_w${DISC_W}_bz${BZ}_${OPT}_sum_loss \
+--output_dir=$ELECTRA_GC/electra_pretrain/${MODEL}_${SIZE}_seq${MAX_SEQ_L}_lr${LR}_w${DISC_W}_bz${BZ}_${OPT}_sampling \
 --optimizer=${OPT} \
 --vocab_file=vocab.txt \
 --disc_loss_weight=${DISC_W} \
