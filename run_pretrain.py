a91e0bfd04fd9bc186190ed1f99bfe52f4cdc3d1
Han Shao <hshao@oberlin.edu>
2020-03-03 15:24:05 -0500

    ele small sampling

--- run_pretrain.py
+++ run_pretrain.py
@@ -276,6 +276,7 @@ def model_fn_builder(electra_config, init_checkpoint, learning_rate,
          masked_lm_positions, masked_lm_ids, masked_lm_weights)
 
         #masked_lm_predictions = tf.argmax(input=masked_logits, axis=-1, output_type=tf.int32)
+        print(masked_logits)
         top_k_candidates = tf.math.top_k(input=masked_logits, k=10)
         random_sample_indice = tf.constant([random.sample(range(0, 9), 1) for i in range(batch_size * seq_length)])
         print(top_k_candidates)
