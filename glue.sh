1c75a36765631aa3ec02046bc2fa5b782cd3f68f
Han Shao <hshao@oberlin.edu>
2020-03-03 16:48:33 -0500

    glue 0short

--- glue.sh
+++ glue.sh
@@ -1,9 +1,9 @@
 ELECTRA_GC='gs://electra'
-TPU_NAME='grpc://10.58.13.82:8470'
+TPU_NAME='grpc://10.73.61.42:8470'
 MODEL=electra
 SIZE=small
 SEED=$$
-CKPT=electra_small_seq128_lr5e-4_w50_bz1024_lamb_0.1short
+CKPT=electra_small_seq128_lr5e-4_w50_bz1024_nolayernorm
 CKPT_NUM=125000
 TASK_INDEX=$1
 
