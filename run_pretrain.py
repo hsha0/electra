a1949e5bfdb7e831343504e71ef285ca528129d5
Han Shao <hshao@oberlin.edu>
2020-03-04 00:10:15 -0500

    test temperature sampling

--- run_pretrain.py
+++ run_pretrain.py
@@ -291,6 +291,8 @@ def model_fn_builder(electra_config, init_checkpoint, learning_rate,
          electra_config, generator.get_sequence_output(), generator.get_embedding_table(),
          masked_lm_positions, masked_lm_ids, masked_lm_weights)
 
+        temperature_sampling(masked_logits, FLAGS.temperature)
+
         top_k_candidates = tf.math.top_k(input=masked_logits, k=10)
         random_sample_indices = tf.constant([random.sample(range(0, 9), 1) for i in range(batch_size * FLAGS.max_predictions_per_seq)])
         masked_lm_predictions = gather_indexes_rank2(top_k_candidates[1], random_sample_indices)
