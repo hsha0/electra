8543c3bc014f375800f3e0d858d1ee3c14e9be41
Han Shao <hshao@oberlin.edu>
2020-03-04 02:00:39 -0500

    temperature sampling

--- pretrain.sh
+++ pretrain.sh
@@ -1,5 +1,5 @@
-TPU_NAME='grpc://10.52.227.130:8470'
-MODEL=ale
+TPU_NAME='grpc://10.121.215.26:8470'
+MODEL=electra
 SIZE=base
 LR=5e-4
 DISC_W=50
@@ -17,7 +17,7 @@ MAX_SEQ_L=128
 python3 run_pretrain.py \
 --electra_config_file=$CONFIG \
 --input_file=$ELECTRA_GC/data_128_CLS_0.1short/*.tfrecord \
---output_dir=$ELECTRA_GC/electra_pretrain/delete_${MODEL}_${SIZE}_seq${MAX_SEQ_L}_lr${LR}_w${DISC_W}_bz${BZ}_${OPT}_T${T}_Tsample \
+--output_dir=$ELECTRA_GC/electra_pretrain/${MODEL}_${SIZE}_seq${MAX_SEQ_L}_lr${LR}_w${DISC_W}_bz${BZ}_${OPT}_T${T}_Tsample \
 --optimizer=${OPT} \
 --temperature=${T} \
 --vocab_file=vocab.txt \
