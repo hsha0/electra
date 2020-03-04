5df75f2a6122676618dfaf58b24c0d1dd9f9d79d
Han Shao <hshao@oberlin.edu>
2020-03-04 01:56:37 -0500

    temperature sampling

--- run_pretrain.py
+++ run_pretrain.py
@@ -234,10 +234,8 @@ def temperature_sampling(logits, temperature):
     if temperature is None or temperature == 0.0:
         return tf.math.argmax(logits)
 
-    preds = tf.math.log(logits + 1e-7) / temperature
-    exp_preds = tf.math.exp(preds)
-    preds = exp_preds / tf.reduce_sum(exp_preds)
-    preds = tf.cast(tf.random.categorical(preds, 1), tf.int32)
+    logits = tf.math.log(logits + 1e-7) / temperature
+    preds = tf.cast(tf.random.categorical(logits, 1), tf.int32)
 
     return preds
 
