d24072548d7f800902cc34a387752f7c8e2c29d6
Han Shao <hshao@oberlin.edu>
2020-03-04 21:15:23 -0500

    glue t1

--- run_pretrain.py
+++ run_pretrain.py
@@ -224,17 +224,57 @@ def replace_elements_by_indices(old, new, indices):
     flat_old_temp = tf.multiply(flat_old, masked_lm_mask)
     new_temp = tf.compat.v1.sparse_to_dense(flat_positions, tf.shape(input=flat_old), new,
                                             default_value=0, validate_indices=True, name=None)
-
     updated_old = tf.reshape(flat_old_temp + new_temp, old_shape)
 
     return updated_old
 
 
+def top_k_logits(logits, k):
+    if k == 0:
+        # no truncation
+        return logits
+
+    def _top_k():
+        values, _ = tf.nn.top_k(logits, k=k)
+        min_values = values[:, -1, tf.newaxis]
+        return tf.where(
+            logits < min_values,
+            tf.ones_like(logits, dtype=logits.dtype) * -1e10,
+            logits,
+        )
+    return tf.cond(
+       tf.equal(k, 0),
+       lambda: logits,
+       lambda: _top_k(),
+    )
+
+
+def top_p_logits(logits, p):
+    """Nucleus sampling"""
+    batch, _ = logits.shape.as_list()
+    sorted_logits = tf.sort(logits, direction='DESCENDING', axis=-1)
+    cumulative_probs = tf.cumsum(tf.nn.softmax(sorted_logits, axis=-1), axis=-1)
+    indices = tf.stack([
+        tf.range(0, batch),
+        # number of indices to include
+        tf.maximum(tf.reduce_sum(tf.cast(cumulative_probs <= p, tf.int32), axis=-1) - 1, 0),
+    ], axis=-1)
+    min_values = tf.gather_nd(sorted_logits, indices)
+    return tf.where(
+        logits < min_values,
+        tf.ones_like(logits) * -1e10,
+        logits,
+    )
+
+
 def temperature_sampling(logits, temperature):
     if temperature is None or temperature == 0.0:
         return tf.math.argmax(logits)
-
-    logits = tf.nn.log_softmax(logits) / temperature
+    logits = logits / temperature
+    logits = top_k_logits(logits, k=0)
+    logits = top_p_logits(logits, p=1)
+    print(logits)
+    sys.exit()
     preds = tf.cast(tf.random.categorical(logits, 1), tf.int32)
 
     return preds
