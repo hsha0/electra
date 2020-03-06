863853e2ec2c2365afd0ee0b8a4bab056b7e453a
Han Shao <hshao@oberlin.edu>
2020-03-06 14:54:36 -0500

    glue adam T1 128

--- glue.sh
+++ glue.sh
@@ -3,7 +3,7 @@ TPU_NAME='grpc://10.124.26.226:8470'
 MODEL=electra
 SIZE=small
 SEED=$$
-CKPT=Tsample_electra_small_seq128_lr5e-4_w50_bz128_lamb_T1
+CKPT=Tsample_electra_small_seq128_lr5e-4_w50_bz128_adam_T1
 CKPT_NUM=125000
 TASK_INDEX=$1
 
