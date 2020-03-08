b3ac135be282179eb8988503100c964d4704c93e
Han Shao <hshao@oberlin.edu>
2020-03-07 21:29:02 -0500

    test seed

--- glue.sh
+++ glue.sh
@@ -6,6 +6,7 @@ SEED=$2
 CKPT=Tsample_electra_small_seq128_lr5e-4_w50_bz1024_lamb_T0.8
 CKPT_NUM=125000
 TASK_INDEX=$1
+RANDOM=$$
 
 TASKS=(MRPC CoLA MNLI SST-2 QQP QNLI WNLI RTE STS-B)
 
@@ -40,7 +41,7 @@ python3 run_classifier.py \
 --electra_config_file=$CONFIG \
 --task_name=${TASK} \
 --data_dir=$ELECTRA_GC/glue/glue_data_new/$TASK \
---output_dir=$ELECTRA_GC/glue/glue_results/${MODEL}_${SIZE}/${TASK}_${SEED} \
+--output_dir=$ELECTRA_GC/glue/glue_results/${MODEL}_${SIZE}/${TASK}_${SEED}_${RANDOM} \
 --init_checkpoint=$INIT_CKPT \
 --vocab_file=vocab.txt \
 --model=$MODEL \
