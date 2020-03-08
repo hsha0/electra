e3b00b0c6271b9132a7ab42b1d8fd59ddc82ae24
Han Shao <hshao@oberlin.edu>
2020-03-08 16:24:16 -0400

    pretrain 3e-4

--- pretrain.sh
+++ pretrain.sh
@@ -1,10 +1,10 @@
-TPU_NAME='grpc://10.62.145.194:8470'
+TPU_NAME='grpc://10.121.169.2:8470'
 MODEL=electra
 SIZE=small
-LR=5e-4
+LR=3e-4
 DISC_W=50
-TRAIN_STEP=250000
-TOTAL=250000
+TRAIN_STEP=125000
+TOTAL=125000
 WARM_UP=10000
 BZ=1024
 OPT=lamb
@@ -17,7 +17,7 @@ MAX_SEQ_L=128
 python3 run_pretrain.py \
 --electra_config_file=$CONFIG \
 --input_file=$ELECTRA_GC/data_128_CLS_0.1short/*.tfrecord \
---output_dir=$ELECTRA_GC/electra_pretrain/250k_${MODEL}_${SIZE}_seq${MAX_SEQ_L}_lr${LR}_w${DISC_W}_bz${BZ}_${OPT}_T${T} \
+--output_dir=$ELECTRA_GC/electra_pretrain/LR3e-4_${MODEL}_${SIZE}_seq${MAX_SEQ_L}_lr${LR}_w${DISC_W}_bz${BZ}_${OPT}_T${T} \
 --optimizer=${OPT} \
 --temperature=${T} \
 --vocab_file=vocab.txt \
