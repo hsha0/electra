75d8acf58f2ad7c3a8babf4cbf5ed4a94256b2b0
Han Shao <hshao@oberlin.edu>
2020-03-08 23:02:29 -0400

    glue 29522

--- pretrain.sh
+++ pretrain.sh
@@ -1,4 +1,4 @@
-TPU_NAME='grpc://10.62.145.194:8470'
+TPU_NAME='grpc://10.115.16.26:8470'
 MODEL=electra
 SIZE=small
 LR=5e-4
@@ -17,7 +17,7 @@ MAX_SEQ_L=128
 python3 run_pretrain.py \
 --electra_config_file=$CONFIG \
 --input_file=$ELECTRA_GC/data_128_CLS_0.1short/*.tfrecord \
---output_dir=$ELECTRA_GC/electra_pretrain/Delete_${MODEL}_${SIZE}_seq${MAX_SEQ_L}_lr${LR}_w${DISC_W}_bz${BZ}_${OPT}_T${T} \
+--output_dir=$ELECTRA_GC/electra_pretrain/29522_${MODEL}_${SIZE}_seq${MAX_SEQ_L}_lr${LR}_w${DISC_W}_bz${BZ}_${OPT}_T${T} \
 --optimizer=${OPT} \
 --temperature=${T} \
 --vocab_file=vocab.txt \
