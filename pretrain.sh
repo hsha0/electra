131fe6e914abbf207e52b53733247d625bd27210
Han Shao <hshao@oberlin.edu>
2020-03-10 17:02:44 -0400

    pretrain 999 T1.2

--- pretrain.sh
+++ pretrain.sh
@@ -1,4 +1,4 @@
-TPU_NAME='grpc://10.27.58.130:8470'
+TPU_NAME='grpc://10.115.192.50:8470'
 MODEL=electra
 SIZE=small
 LR=5e-4
@@ -8,7 +8,7 @@ TOTAL=125000
 WARM_UP=10000
 BZ=1024
 OPT=lamb
-T=1
+T=1.2
 
 ELECTRA_GC='gs://electra'
 CONFIG=config/${MODEL}_${SIZE}.json
