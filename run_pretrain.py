0f71c38324693fd75f2ea45b97c52b2f66470c3f
Han Shao <hshao@oberlin.edu>
2020-03-11 02:17:50 -0400

    sampling

--- run_pretrain.py
+++ run_pretrain.py
@@ -235,10 +235,7 @@ def temperature_sampling(logits, temperature):
         return tf.math.argmax(logits)
 
     log_prob = tf.nn.log_softmax(logits / temperature)
-    log_prob_shape = modeling.get_shape_list(log_prob)
-
-    log_prob = tf.slice(log_prob, [0, 999], [log_prob_shape[0], log_prob_shape[1]-999])
-    preds = tf.cast(tf.random.categorical(log_prob, 1), tf.int32) + 999
+    preds = tf.cast(tf.random.categorical(log_prob, 1), tf.int32)
     return preds
 
 
