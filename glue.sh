ac74dc7fadcceb3bf4a26b71ad52bff957b09fd7
Han Shao <hshao@oberlin.edu>
2020-03-10 20:21:09 -0400

    glue t1.2

--- glue.sh
+++ glue.sh
@@ -3,7 +3,7 @@ TPU_NAME='grpc://10.83.178.34:8470'
 MODEL=electra
 SIZE=small
 SEED=$$
-CKPT=Tsample_electra_small_seq128_lr5e-4_w50_bz1024_lamb_T0.5
+CKPT=Tsample_electra_small_seq128_lr5e-4_w50_bz1024_lamb_T1.2
 CKPT_NUM=125000
 TASK_INDEX=$1
 
